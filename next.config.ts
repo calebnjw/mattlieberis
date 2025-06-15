adff6d53e2482644642b9f7b4d610360d28cdfb8
Caleb <5322420+calebnjw@users.noreply.github.com>
2025-06-16 00:30:22 +0800

    add check for production environment

--- next.config.ts
+++ next.config.ts
@@ -1,12 +1,15 @@
 import type { NextConfig } from "next";
 
+const isProd = process.env.NODE_ENV === "production";
+
 const nextConfig: NextConfig = {
   /* config options here */
-  // output: "export",
+  basePath: isProd ? "/matt-lieber-is" : "",
+  output: "export",
   distDir: "build",
-  // images: {
-  //   unoptimized: true,
-  // },
+  images: {
+    unoptimized: true,
+  },
 };
 
 export default nextConfig;
