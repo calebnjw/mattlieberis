983a5f137769515d5750dfe3bfc1d706ad7ff8af
Caleb <5322420+calebnjw@users.noreply.github.com>
2025-06-16 00:12:28 +0800

    change cursor for theme toggle button

--- src/components/themeToggle.tsx
+++ src/components/themeToggle.tsx
@@ -11,7 +11,7 @@ export function ThemeToggle() {
     <Button
       variant="ghost"
       onClick={() => setTheme(theme === "light" ? "dark" : "light")}
-      className="rounded-full p-1 hover:bg-neutral-100 dark:hover:bg-neutral-800"
+      className="rounded-full p-1 hover:bg-neutral-100 dark:hover:bg-neutral-800 cursor-pointer"
     >
       <Sun className="size-4 rotate-0 scale-100 transition-all dark:-rotate-90 dark:scale-0" />
       <Moon className="absolute size-4 rotate-90 scale-0 transition-all dark:rotate-0 dark:scale-100" />
