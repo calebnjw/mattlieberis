e5adc7ea34ebed15245b70d4f15f28fdf623f274
Caleb <5322420+calebnjw@users.noreply.github.com>
2025-07-14 20:04:39 +0800

    isProd should check if environment is produdction.

--- next.config.ts
+++ next.config.ts
@@ -1,7 +1,7 @@
 import type { NextConfig } from "next";
 
-// const isProd = process.env.NODE_ENV === "production";
-const isProd = true;
+const isProd = process.env.NODE_ENV === "production";
+// const isProd = true;
 
 const nextConfig: NextConfig = {
   /* config options here */
